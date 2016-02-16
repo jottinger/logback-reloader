df158dd692165675e9d302ca71af6964b1599fbf
Joseph B. Ottinger <joseph@perahealth.com>
2016-02-16 09:34:33 -0500

    Fixing various warnings, cleaning up log files

--- src/main/java/com/autumncode/components/coarse/CoarseLogThingImpl.java
+++ src/main/java/com/autumncode/components/coarse/CoarseLogThingImpl.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2016, Joseph B. Ottinger
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
 package com.autumncode.components.coarse;
 
 import com.autumncode.components.LogThing;
@@ -5,9 +21,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+@SuppressWarnings("unused")
 @Component("coarseLogThing")
 public class CoarseLogThingImpl implements LogThing {
-    static final Logger log = LoggerFactory.getLogger(CoarseLogThingImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(CoarseLogThingImpl.class);
 
     @Override
     public Logger getLog() {
--- src/test/java/com/autumncode/logger/InMemoryAppender.java
+++ src/test/java/com/autumncode/logger/InMemoryAppender.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2016, Joseph B. Ottinger
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
 package com.autumncode.logger;
 
 import ch.qos.logback.classic.spi.LoggingEvent;
@@ -6,24 +22,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InMemoryAppender extends AppenderBase {
-    static List<String> logMessages = new ArrayList<>();
+    private static final List<String> logMessages = new CopyOnWriteArrayList<>();
 
-    public synchronized void reset() {
-        logMessages.clear();
+    public void reset() {
+
+        synchronized (logMessages) {
+            logMessages.clear();
+        }
     }
 
     public List<String> getLogMessages() {
-        return logMessages;
+        synchronized (logMessages) {
+            return new ArrayList<>(logMessages);
+        }
     }
 
     @Override
     protected void append(Object o) {
         if (o instanceof LoggingEvent) {
             LoggingEvent le = (LoggingEvent) o;
-            String message=le.getLoggerName() + ":" + le.getFormattedMessage();
+            String message = le.getLoggerName() + ":" + le.getFormattedMessage();
             logMessages.add(message);
             System.out.println(message);
         }
